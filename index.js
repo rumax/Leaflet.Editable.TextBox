bbb5da0920bf64f73e36aa6c35ee04ad98a66ce4
Maksym Rusynyk <m.rusynyk@gmail.com>
2016-04-20 13:20:00 +0200

    minor fix (eslint)

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+
 var L = require('leaflet');
 require('leaflet-editable');
 require('leaflet-path-transform/src/Matrix');
 require('./src/Textbox');
 require('./src/Util');
-require('./src/Editable.Textbox')
+require('./src/Editable.Textbox');
 
 module.exports = L.Editable.TextBox;
